33ebfef38c87d37e09e32add79b6ce866be0ca7c
Koen de Raden <153603469+koenderaden@users.noreply.github.com>
2024-09-17 16:26:34 +0200

    Contact, About en Header
    Deze pagina's/components toegevoegd

--- page.jsx
+++ page.jsx
@@ -1,18 +1,22 @@
-export default function Home() {
+import React from 'react'
+
+const page = () => {
   return (
-      <main className="grid">
-        <img src="https://picsum.photos/id/267/700/475" 
-        alt="beach" />
-        <div className="welcome">
-          <h2>Welcome</h2>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
-            cumque molestiae culpa nisi voluptatibus laudantium mollitia
-            obcaecati omnis in, ea adipisci, deleniti praesentium? Similique
-            aspernatur amet dolor illum, ea tempore animi laborum. Temporibus
-            explicabo, omnis molestiae cum eos quidem officia.
-          </p>
-        </div>
-      </main>
+    <main className="grid">
+      <img src="https://picsum.photos/id/876/700/475" 
+      alt="beach" />
+      <div className="welcome">
+        <h2>Welcome</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus,
+          cumque molestiae culpa nisi voluptatibus laudantium mollitia obcaecati
+          omnis in, ea adipisci, deleniti praesentium? Similique aspernatur amet
+          dolor illum, ea tempore animi laborum. Temporibus explicabo, omnis
+          molestiae cum eos quidem officia.
+        </p>
+      </div>
+    </main>
   );
 };
+
+export default page
